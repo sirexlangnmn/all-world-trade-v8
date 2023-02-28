cb1297de16c1dee6d2f3a1ef7aaaa6b343b5bbfa
Federex A Potolin <36563535+sirexlangnmn@users.noreply.github.com>
2023-03-01 01:40:52 +0800

    trying to fix the bug in selection image when searchin

--- public/assets/js/selection.js
+++ public/assets/js/selection.js
@@ -526,11 +526,11 @@ function displayTopCompanyImage() {
     // companiesProfilePicture.appendChild(fragment);
     companiesProfilePicture.innerHTML = '';
     
-        let bannerTitle = getCompaniesProfilePicture(companyDetailsJsonObj2[0][0].id, companyDetailsJsonObj2[0][0].uuid);
-        console.log('displayTopCompanyImage bannerTitle1:', bannerTitle);
+        let bannerTitle1 = getCompaniesProfilePicture(companyDetailsJsonObj2[0][0].id, companyDetailsJsonObj2[0][0].uuid);
+        console.log('displayTopCompanyImage bannerTitle1:', bannerTitle1);
         console.log('displayTopCompanyImage business_name2:', companyDetailsJsonObj2[0][0].business_name);
         const img = new Image();
-        img.src = host + '/uploads/users_upload_files/' + bannerTitle[0].banner;
+        img.src = host + '/uploads/users_upload_files/' + bannerTitle1[0].banner;
         img.onload = function() {
             const adjustedScreenHeight = window.innerHeight - 125;
             img.style.height = `${adjustedScreenHeight}px`;
@@ -541,19 +541,22 @@ function displayTopCompanyImage() {
 
         setTimeout(() => {            
             const leng = companyDetailsJsonObj2[0].length;
+            // for (let i = leng - 1; i >= 0; i--) {
             for (var i = leng - 1; i > -1; i--) {
-                let bannerTitle = getCompaniesProfilePicture(companyDetailsJsonObj2[0][i].id, companyDetailsJsonObj2[0][i].uuid);
-                console.log('displayTopCompanyImage bannerTitle2:', i +' - '+ bannerTitle);
-                console.log('displayTopCompanyImage business_name2:', i +' - '+ companyDetailsJsonObj2[0][i].business_name);
-                const img = new Image();
-                img.src = host + '/uploads/users_upload_files/' + bannerTitle[0].banner;
-                img.onload = function() {
-                    const adjustedScreenHeight = window.innerHeight - 125;
-                    img.style.height = `${adjustedScreenHeight}px`;
-                    const li = document.createElement('li');
-                    li.appendChild(img);
-                    companiesProfilePicture.appendChild(li);
-                };
+                let bannerTitle2 = getCompaniesProfilePicture(companyDetailsJsonObj2[0][i].id, companyDetailsJsonObj2[0][i].uuid);
+                if(bannerTitle1 != bannerTitle2) {
+                    console.log('displayTopCompanyImage bannerTitle2:', i +' - '+ bannerTitle2);
+                    console.log('displayTopCompanyImage business_name2:', i +' - '+ companyDetailsJsonObj2[0][i].business_name);
+                    const img = new Image();
+                    img.src = host + '/uploads/users_upload_files/' + bannerTitle2[0].banner;
+                    img.onload = function() {
+                        const adjustedScreenHeight = window.innerHeight - 125;
+                        img.style.height = `${adjustedScreenHeight}px`;
+                        const li = document.createElement('li');
+                        li.appendChild(img);
+                        companiesProfilePicture.appendChild(li);
+                    };
+                }
             }
         }, 500);
         
