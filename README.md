28f7e449ca7706db57879ebc019f3f20b0aec59c
Federex A Potolin <36563535+sirexlangnmn@users.noreply.github.com>
2023-02-28 15:25:20 +0800

    trying to fix the bug in selection image when searchin

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 function selectionSearchParameter() {
 function displayTopCompany() {
-  
+
 for (var i = data.length - 1; i > -1; i--) {
                     let bannerTitle = getCompaniesProfilePicture(data[i]['id'], data[i]['uuid']);
                     const img = new Image();
@@ -13,3 +13,21 @@ for (var i = data.length - 1; i > -1; i--) {
                         companiesProfilePicture.appendChild(li);
                     };
                 }
+
+
+
+
+
+
+for (let i = leng - 1; i >= 0; i--) {
+	let bannerTitle = getCompaniesProfilePicture(companyDetailsJsonObj2[0][i].id, companyDetailsJsonObj2[0][i].uuid);
+	const img = new Image();
+	img.src = host + '/uploads/users_upload_files/' + bannerTitle[0].banner;
+	img.onload = function() {
+		const adjustedScreenHeight = window.innerHeight - 125;
+		img.style.height = `${adjustedScreenHeight}px`;
+		const li = document.createElement('li');
+		li.appendChild(img);
+		companiesProfilePicture.appendChild(li);
+	};
+}
--- public/assets/js/selection.js
+++ public/assets/js/selection.js
@@ -486,7 +486,20 @@ function displayTopCompany() {
             </div>`;
     }
     
-    for (let i = 0; i < leng; i++) {
+    // for (let i = leng - 1; i >= 0; i--) {
+    //     let bannerTitle = getCompaniesProfilePicture(companyDetailsJsonObj2[0][i].id, companyDetailsJsonObj2[0][i].uuid);
+    //     const img = new Image();
+    //     img.src = host + '/uploads/users_upload_files/' + bannerTitle[0].banner;
+    //     img.onload = function() {
+    //       const adjustedScreenHeight = window.innerHeight - 125;
+    //       img.style.height = `${adjustedScreenHeight}px`;
+    //       const li = document.createElement('li');
+    //       li.appendChild(img);
+    //       companiesProfilePicture.appendChild(li);
+    //     };
+    // }
+
+    for (let i = leng - 1; i >= 0; i--) {
         let bannerTitle = getCompaniesProfilePicture(companyDetailsJsonObj2[0][i].id, companyDetailsJsonObj2[0][i].uuid);
         const img = new Image();
         img.src = host + '/uploads/users_upload_files/' + bannerTitle[0].banner;
@@ -495,9 +508,10 @@ function displayTopCompany() {
           img.style.height = `${adjustedScreenHeight}px`;
           const li = document.createElement('li');
           li.appendChild(img);
-          companiesProfilePicture.appendChild(li);
+          companiesProfilePicture.insertBefore(li, companiesProfilePicture.firstChild);
         };
     }
+      
       
     
 }
