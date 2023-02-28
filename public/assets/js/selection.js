b512f8c9a6cdea1b2d0e62573b80ec156f554e99
Federex A Potolin <36563535+sirexlangnmn@users.noreply.github.com>
2023-02-28 16:00:37 +0800

    trying to fix the bug in selection image when searchin

--- public/assets/js/selection.js
+++ public/assets/js/selection.js
@@ -500,8 +500,7 @@ function displayTopCompany() {
               img.style.height = `${adjustedScreenHeight}px`;
               const li = document.createElement('li');
               li.appendChild(img);
-            //   companiesProfilePicture.insertBefore(li, companiesProfilePicture.firstChild);
-            companiesProfilePicture.appendChild(li);
+              companiesProfilePicture.insertBefore(li, companiesProfilePicture.firstChild);
             };
         }
     // }
