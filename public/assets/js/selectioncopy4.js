e3192cb72e101258df3f4e62e6af2f07766094d9
Federex A Potolin <36563535+sirexlangnmn@users.noreply.github.com>
2023-02-28 18:15:20 +0800

    trying to fix the bug in selection image when searchin

--- public/assets/js/selectioncopy4.js
+++ public/assets/js/selectioncopy4.js
@@ -1153,19 +1153,43 @@ function selectionSearchParameter() {
 
                 companiesProfilePicture.innerHTML = '';
             
-                const fragment = document.createDocumentFragment();
+                // for (var i = data.length - 1; i > -1; i--) {
+                //     let bannerTitle = getCompaniesProfilePicture(data[i]['id'], data[i]['uuid']);
 
-                for (let i = data.length - 1; i >= 0; i--) {
-                    const bannerTitle = getCompaniesProfilePicture(data[i]['id'], data[i]['uuid']);
-                    const bannerSrc = host + '/uploads/users_upload_files/' + bannerTitle[0].banner;
-                  
-                    img.src = bannerSrc;
-                    const li = document.createElement('li');
-                    li.appendChild(img.cloneNode());
-                    fragment.appendChild(li);
+                //     companiesProfilePicture.innerHTML =
+                //         companiesProfilePicture.innerHTML +
+                //         '<li>' +
+                //         '<img src="' +
+                //         host +
+                //         '/uploads/users_upload_files/' +
+                //         bannerTitle[0].banner +
+                //         '" alt="" uk-cover>' +
+                //         '</li>';
+
+                //     if(i === 0) {
+                //         setTimeout(() => {
+                //             const adjustedScreenHeight = window.innerHeight - 125;
+                //             // Get the image element
+                //             const img = document.querySelector('#companiesProfilePicture li img');
+                //             // Remove the height style property
+                //             img.style.height = `${adjustedScreenHeight}px`;
+                //         }, 500);
+                //     }
+                // }
+
+
+                for (var i = data.length - 1; i > -1; i--) {
+                    let bannerTitle = getCompaniesProfilePicture(data[i]['id'], data[i]['uuid']);
+                    const img = new Image();
+                    img.src = host + '/uploads/users_upload_files/' + bannerTitle[0].banner;
+                    img.onload = function() {
+                        const adjustedScreenHeight = window.innerHeight - 125;
+                        img.style.height = `${adjustedScreenHeight}px`;
+                        const li = document.createElement('li');
+                        li.appendChild(img);
+                        companiesProfilePicture.appendChild(li);
+                    };
                 }
-                  
-                companiesProfilePicture.appendChild(fragment);
 
                 displayFirstCompanyDetails();
                 displayTopCompany();
