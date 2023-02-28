019c046bd1870e172220501731f64b1c4ec15122
Federex A Potolin <36563535+sirexlangnmn@users.noreply.github.com>
2023-02-28 16:32:40 +0800

    trying to fix the bug in selection image when searchin

--- public/assets/js/selection.js
+++ public/assets/js/selection.js
@@ -488,18 +488,18 @@ function displayTopCompany() {
     }
    
     
-    for (let i = leng - 1; i > -1; i--) {
-        let bannerTitle = getCompaniesProfilePicture(companyDetailsJsonObj2[0][i].id, companyDetailsJsonObj2[0][i].uuid);
-        const img = new Image();
-        img.src = host + '/uploads/users_upload_files/' + bannerTitle[0].banner;
-        img.onload = function() {
-            const adjustedScreenHeight = window.innerHeight - 125;
-            img.style.height = `${adjustedScreenHeight}px`;
-            const li = document.createElement('li');
-            li.appendChild(img);
-            companiesProfilePicture.insertBefore(li, companiesProfilePicture.firstChild);
-        };
-    }
+    // for (let i = leng - 1; i > -1; i--) {
+    //     let bannerTitle = getCompaniesProfilePicture(companyDetailsJsonObj2[0][i].id, companyDetailsJsonObj2[0][i].uuid);
+    //     const img = new Image();
+    //     img.src = host + '/uploads/users_upload_files/' + bannerTitle[0].banner;
+    //     img.onload = function() {
+    //         const adjustedScreenHeight = window.innerHeight - 125;
+    //         img.style.height = `${adjustedScreenHeight}px`;
+    //         const li = document.createElement('li');
+    //         li.appendChild(img);
+    //         companiesProfilePicture.insertBefore(li, companiesProfilePicture.firstChild);
+    //     };
+    // }
    
 }
 
